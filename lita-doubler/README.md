bd58374faaa524bb9694ada1aeca1644c5ef79a4
ym-Rain <ym_Rain@163.com>
2020-12-16 16:10:20 +0800

    chatbot

---
+++ lita-doubler/README.md
@@ -0,0 +1,22 @@
+# lita-doubler
+
+[![Build Status](https://travis-ci.org/ym-Rain/lita-doubler.png?branch=master)](https://travis-ci.org/ym-Rain/lita-doubler)
+[![Coverage Status](https://coveralls.io/repos/ym-Rain/lita-doubler/badge.png)](https://coveralls.io/r/ym-Rain/lita-doubler)
+
+TODO: Add a description of the plugin.
+
+## Installation
+
+Add lita-doubler to your Lita instance's Gemfile:
+
+``` ruby
+gem "lita-doubler"
+```
+
+## Configuration
+
+TODO: Describe any configuration attributes the plugin exposes.
+
+## Usage
+
+TODO: Describe the plugin's features and how to use them.
---
+++ lita-doubler/spec/lita/handlers/doubler_spec.rb
@@ -0,0 +1,33 @@
+require "spec_helper"
+
+describe Lita::Handlers::Doubler, lita_handler: true do
+ let(:robot){Lita::Robot.new(registry)}
+
+ subject{described_class.new(robot)}
+
+ describe 'routing' do
+  it{is_expected.to route('Lita double 2')}
+  it{is_expected.to route('Lita double   22')}
+  it{is_expected.to route('Lita double 4')}
+
+  it{is_expected.to_not route('Lita double two')}
+  it{is_expected.to_not route('Lita double 1e4')}
+ end
+
+ describe 'functionality' do
+  describe ':double_number' do
+   let(:n){rand(1..100)}
+   it 'returns double the input' do
+    actual = subject.double_number n
+    expected = n*2
+
+    expected(actual).to eq(expected)
+   end
+  end
+ 
+  it 'doubles numbers when asked to' do 
+   send_message 'Lita double 2'
+   expected(replies.last).to eq('2+2=4')
+  end
+ end
+end
